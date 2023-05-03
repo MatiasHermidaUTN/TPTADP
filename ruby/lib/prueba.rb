5f5fad227330707f2cd3be754ee426a099697731
matiascarasi <mcarasi@frba.utn.edu.ar>
2023-05-03 19:46:03 -0300

    aislamiento en mixin Persistent

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -12,60 +12,7 @@ class FalseClass
   include Boolean
 end
 
-class Module
-  def has_one(type, description)
-    #TODO: Agregar validaciones si es de tipo persistible
-    name = description[:named]
-    attr_accessor name
-    attr_options_hash = {name => { :type => type }}
-    persistent_attrs_hash = singleton_methods(false).include?(:persistent_attrs) ? singleton_method(:persistent_attrs).call.merge(attr_options_hash) : attr_options_hash
-    define_singleton_method(:persistent_attrs) do
-      persistent_attrs_hash
-    end
-  end
-
-  def all_instances
-    table = TADB::DB.table(self.to_s)
-    obj = self.new
-    obj.define_singleton_method(:id=) do |id|
-      id
-    end
-    table.entries.collect do |attributes|
-      attributes.each do |attr, value|
-        if attr === :id
-          obj.define_singleton_method(:id) do
-            value
-          end
-        end
-        obj.send(attr.to_s+"=", value)
-      end
-      obj
-    end
-  end
-
-  private def responds_to_find_by(name)
-    name.to_s.start_with?("find_by_")
-  end
-
-  def method_missing(name, *args)
-    if responds_to_find_by(name)
-      value = args[0]
-      message = name.to_s.delete_prefix("find_by_")
-      self.all_instances.select do |obj|
-        obj.send(message) === value
-      end
-    else
-      super
-    end
-  end
-
-  def respond_to_missing?(name, include_private = false)
-    responds_to_find_by(name) || super
-  end
-
-end
-class Object
-
+module Persistent
   @@simple_types = [Numeric, String, Boolean]
   def save!
 
@@ -98,9 +45,9 @@ class Object
 
   def refresh!
     table = TADB::DB.table(self.class.to_s)
-    entry = table.entries.find { |entry| entry[:id] === id }
+    entry = table.entries.find { |entry| entry[:id] == id }
     entry.each do |attr, value|
-      if attr === :id
+      if attr == :id
         next
       end
       type = self.class.singleton_method(:persistent_attrs).call[attr][:type]
@@ -116,8 +63,71 @@ class Object
     define_singleton_method(:id) do
       nil
     end
+    self.class.singleton_method(:persistent_attrs).call.each do |name,options|
+      if @@simple_types.include?(options[:type])
+        next
+      end
+      send(name).forget!
+    end
   end
 end
+
+class Module
+
+  def has_one(type, description)
+    #TODO: Agregar validaciones si es de tipo persistible
+    name = description[:named]
+    attr_accessor name
+    attr_options_hash = {name => { :type => type }}
+    # Si ya existe el metodo persistent_attrs (es decir, ya se agrego con has_one otro atributo) se mergea al estado actual del hash. Si es falso, se inicializa con la variable attr_options_hash
+    persistent_attrs_hash = singleton_methods(false).include?(:persistent_attrs) ? singleton_method(:persistent_attrs).call.merge(attr_options_hash) : attr_options_hash
+    define_singleton_method(:persistent_attrs) do
+      persistent_attrs_hash
+    end
+    self.send(:include, Persistent)
+  end
+
+  def all_instances
+    table = TADB::DB.table(self.to_s)
+    obj = self.new
+    obj.define_singleton_method(:id=) do |id|
+      id
+    end
+    table.entries.collect do |attributes|
+      attributes.each do |attr, value|
+        if attr == :id
+          obj.define_singleton_method(:id) do
+            value
+          end
+        end
+        obj.send(attr.to_s+"=", value)
+      end
+      obj
+    end
+  end
+
+  private def responds_to_find_by(name)
+    name.to_s.start_with?("find_by_")
+  end
+
+  def method_missing(name, *args)
+    if responds_to_find_by(name)
+      value = args[0]
+      message = name.to_s.delete_prefix("find_by_")
+      self.all_instances.select do |obj|
+        obj.send(message) == value
+      end
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(name, include_private = false)
+    responds_to_find_by(name) || super
+  end
+
+end
+
 class Grade
   has_one Numeric, named: :value
 end
@@ -137,6 +147,7 @@ g.value
 g.value = 5
 g.save!
 s.refresh!.grade.value # Retorna Grade(5)
+s.forget!
 
 =begin
 class Person
