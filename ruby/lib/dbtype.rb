672394d3fdd77e75aa5612c58e86ad75c59c3b07
ketzeLitvak <elitvak@frba.utn.edu.ar>
2023-05-23 00:20:11 -0300

    Se agrega polimorfismo para el refresh! y el save!, se hace una tabla intermedia real para las relaciones many

--- ruby/lib/dbtype.rb
+++ ruby/lib/dbtype.rb
@@ -1,5 +1,33 @@
 require_relative './validations'
 
+module ComplexType
+    def validate_complex!(value, attr_name)
+        raise AttrNotCorrectTypeError.new(attr_name) unless value.is_a?(@type)
+        if (!@validations.nil?)
+            value.class.persistent_attributes.each do |key, db_type|
+                if (key != :id)
+                    db_type.validate!(value.attributes_hash, key)
+                end
+            end
+        end
+    end
+end
+
+module SimpleType
+    def validate_simple!(value, attr_name)
+        raise AttrNotCorrectTypeError.new(attr_name) unless value.is_a?(@type)
+        if (!@validations.nil?)
+            @validations.each do |validator|
+                validator.validate!(value)
+            end
+        end
+    end
+
+    def save!(hash_all_attr, attr_name, instance)
+        hash_all_attr
+    end
+end
+
 module DbType
     attr_accessor :name, :type, :validations, :default
     def initialize(type, description)
@@ -22,71 +50,91 @@ module DbType
             result
         }
     end
+
+    def validate_default!(hash_all_attr, attr_name)
+        if hash_all_attr[attr_name].nil? && !@default.nil? then hash_all_attr[attr_name] = @default end
+    end
 end
+
 class OneComplexDbType
     include DbType
+    include ComplexType
     def validate!(hash_all_attr, attr_name)
-        value = hash_all_attr[attr_name]
-        if value.nil? && !@default.nil? then value = hash_all_attr[attr_name] = @default end
-        raise AttrNotCorrectTypeError.new(attr_name) unless value.is_a?(@type)
-        if (!@validations.nil?)
-            value.class.persistent_attributes.each do |key, db_type|
-                if (key != :id)
-                    db_type.validate!(value.attributes_hash, key)
-                end
-            end
-        end
+        validate_default!(hash_all_attr, attr_name)
+        validate_complex!(hash_all_attr[attr_name], attr_name)
         hash_all_attr
+    end
+
+    def save!(hash_all_attr, attr_name, instance)
+        hash_all_attr[attr_name] = hash_all_attr[attr_name].save!
+        hash_all_attr
+    end
+
+    def refresh!(saved_value, attr_name, instance)
+        instance.send(attr_name.to_s + "=", @type.find_by_id(saved_value).first)
     end
 end
 
 class ManyComplexDbType
     include DbType
+    include ComplexType
     def validate!(hash_all_attr, attr_name)
-        array = hash_all_attr[attr_name]
-        if array.nil? && !@default.nil? then array = hash_all_attr[attr_name] = @default end
-        array.each do |value|
-            raise AttrNotCorrectTypeError.new(attr_name) unless value.is_a?(@type)
-            if (!@validations.nil?)
-                value.class.persistent_attributes.each do |key, db_type|
-                    if (key != :id)
-                        db_type.validate!(value.attributes_hash, key)
-                    end
-                end
-            end
+        validate_default!(hash_all_attr, attr_name)
+        hash_all_attr[attr_name].each do |value|
+            validate_complex!(value, attr_name)
         end
         hash_all_attr
+    end
+
+    def save!(hash_all_attr, attr_name, instance)
+        hash_all_attr[:id] ||= SecureRandom.uuid
+        hash_all_attr[attr_name].each do |value|
+            id_complex = value.save!
+            intermediate_table(instance.class.name).insert({ instance.class.name.to_sym => hash_all_attr[:id], self.class.name.to_sym  => id_complex })
+        end
+        hash_all_attr.except(attr_name)
+    end
+
+    def refresh!(saved_value, attr_name, instance)
+        ids = intermediate_table(instance.class.name).entries.select {|entry| entry[instance.class.name.to_sym] == instance.id}
+        values = ids.reduce([]) { |result, entry|
+            result << @type.find_by_id(entry[self.class.name.to_sym]).first
+            result
+        }
+        instance.send(attr_name.to_s + "=", values)
+    end
+
+    def intermediate_table(class_name)
+        TADB::DB.table(self.class.name + class_name)
     end
 end
 
 class OneSimpleDbType
     include DbType
+    include SimpleType
     def validate!(hash_all_attr, attr_name)
-        value = hash_all_attr[attr_name]
-        if value.nil? && !@default.nil? then value = hash_all_attr[attr_name] = @default end
-        raise AttrNotCorrectTypeError.new(attr_name) unless value.is_a?(@type)
-        if (!@validations.nil?)
-            @validations.each do |validator|
-                validator.validate!(value)
-            end
-        end
+        validate_default!(hash_all_attr, attr_name)
+        validate_simple!(hash_all_attr[attr_name], attr_name)
         hash_all_attr
+    end
+
+    def refresh!(saved_value, attr_name, instance)
+        instance.send(attr_name.to_s + "=", saved_value)
     end
 end
 
 class ManySimpleDbType
     include DbType
+    include SimpleType
     def validate!(hash_all_attr, attr_name)
-        array = hash_all_attr[attr_name]
-        if array.nil? && !@default.nil? then array = hash_all_attr[attr_name] = @default end
-        array.each do |value|
-            raise AttrNotCorrectTypeError.new(attr_name) unless value.is_a?(@type)
-            if (!@validations.nil?)
-                @validations.each do |validator|
-                    validator.validate!(value)
-                end
-            end
+        validate_default!(hash_all_attr, attr_name)
+        hash_all_attr[attr_name].each do |value|
+            validate_simple!(value, attr_name)
         end
         hash_all_attr
+    end
+
+    def refresh!(saved_value, attr_name, instance)
+        instance.send(attr_name.to_s + "=", saved_value)
     end
 end
--- ruby/lib/persistent.rb
+++ ruby/lib/persistent.rb
@@ -15,44 +15,11 @@ module Persistent
   def save!
     hash_all_attr = self.attributes_hash
     self.validate!(hash_all_attr)
-
-    save_complex!(hash_all_attr)
-    save_many!(hash_all_attr)
-
     table.delete(id) unless id.nil?
+    self.class.persistent_attributes.each do |attr_name, db_type|
+      hash_all_attr = db_type.save!(hash_all_attr, attr_name, self)
+    end
     self.id = table.insert(hash_all_attr)
-  end
-
-  def save_many!(hash_all_attr)
-    hash_many_attr = hash_all_attr.select do |attr_name, value| value.is_a?(Array) end  # {full_names => ["Juan", "Pedro"], grades => [grade1, grade2]}
-    hash_many_attr.each do |attr_name, array_values|  # attr_name = grades, array_values = [grade1, grade2]
-      # if array_values.empty?
-      #   all_attr_values.store(attr_name, 0)
-      #   next
-      # end
-      i=0
-      hash_values = array_values.to_h do |attr| ["#{attr_name}_#{i += 1}", attr] end  # {grades_1 => grade1, grades_2 => grade2}
-
-      db_type = self.class.persistent_attributes[attr_name]
-      if db_type.class == OneComplexDbType || db_type.class == ManyComplexDbType
-        hash_values.each do |attr_name1, value|  # {grades_1 => grade1, grades_2 => grade2}
-          id_complex = value.save!    #save! devuelve id de la tabla del attr complejo
-          hash_values[attr_name1] = id_complex  # {grades_1 => id1, grades_2 => id2}
-        end
-      end
-
-      id_many = TADB::DB.table(attr_name).insert(hash_values)
-      hash_all_attr.store(attr_name, id_many)
-    end
-  end
-
-  def save_complex!(hash_all_attr)
-    self.get_complex_attributes.each do |attr_name, type|
-      unless hash_all_attr[attr_name].is_a?(Array)
-        id_complex = hash_all_attr[attr_name].save! #save! devuelve id de la tabla del attr complejo
-        hash_all_attr[attr_name] = id_complex
-      end
-    end
   end
 
   def refresh!
@@ -60,24 +27,9 @@ module Persistent
     saved_obj = table.entries.find {|entry| entry[:id] == self.id}
     self.class.persistent_attributes.each do |attr_name, db_type|
       saved_value = saved_obj[attr_name]
-      if self.get_has_many_attributes.include?(attr_name)
-        self.refresh_many!(attr_name, db_type.type, saved_value)
-      else
-        value = db_type.class == OneComplexDbType || db_type.class == ManyComplexDbType ? db_type.type.find_by_id(saved_value).first : saved_value  #agrego el first pq find_by_id devuelve un array
-        self.send(attr_name.to_s + "=", value)
-      end
+      db_type.refresh!(saved_value, attr_name, self)
     end
     self
-  end
-
-  def refresh_many!(attr_name, type_attr, id_many)
-    hash_attr = TADB::DB.table(attr_name).entries.select{ |entry| entry[:id] == id_many }.first
-    self.send(attr_name.to_s + "=", [])
-    hash_attr.keys.each do |key|
-      if key == :id then next end
-      value = self.class.is_complex_type?(type_attr) ? type_attr.find_by_id(hash_attr[key]).first : hash_attr[key]
-      self.send(attr_name.to_s).push(value)
-    end
   end
 
   def forget!
@@ -88,18 +40,6 @@ module Persistent
 
   def table
     TADB::DB.table(self.class.name)
-  end
-
-  def get_complex_attributes
-    self.class.persistent_attributes.select do | attr_name, db_type|
-      db_type.class == OneComplexDbType || db_type.class == ManyComplexDbType
-    end
-  end
-
-  def get_has_many_attributes
-    self.class.persistent_attributes.select do |attr_name, db_type|
-      self.send(attr_name).is_a?(Array)
-    end
   end
 
   def attributes_hash
